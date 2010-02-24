6c8157405edc2cd90eeb450608bd4c138477fb2d
lukas <lukas.westermann@gmail.com>
2010-02-24 12:42:59 +0100

    for rails 2.x compat

---
+++ rails/init.rb
@@ -0,0 +1,4 @@
+require 'vacuum_cleaner'
+
+ActiveRecord::Base.class_eval { include VacuumCleaner::Normalizations } # all versions of rails
+ActiveModel::Base.class_eval { include VacuumCleaner::Normalizations } if defined?(ActiveModel::Base) # Rails 3+

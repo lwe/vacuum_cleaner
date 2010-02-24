cfb821d3bc6a9272d96965bfc62f89c88b181aec
lukas <lukas.westermann@gmail.com>
2010-02-24 15:05:07 +0100

    use only a single rails init.rb file and removed support for ActiveModel, this is then opt-in, like when using POROs

--- init.rb
+++ init.rb
@@ -1,4 +1,1 @@
-require 'vacuum_cleaner'
-
-ActiveRecord::Base.class_eval { include VacuumCleaner::Normalizations } # all versions of rails
-ActiveModel::Base.class_eval { include VacuumCleaner::Normalizations } if defined?(ActiveModel::Base) # Rails 3+
+require File.dirname(__FILE__) + "rails/init"
--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,4 @@
 require 'vacuum_cleaner'
 
 ActiveRecord::Base.class_eval { include VacuumCleaner::Normalizations } # all versions of rails
-ActiveModel::Base.class_eval { include VacuumCleaner::Normalizations } if defined?(ActiveModel::Base) # Rails 3+
+# ActiveModel::Base.class_eval { include VacuumCleaner::Normalizations } if defined?(ActiveModel::Base) # Rails 3+

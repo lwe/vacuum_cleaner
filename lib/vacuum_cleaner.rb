c770bb2053e1c181f4b4329ea9539180e2cf364a
lukas <lukas.westermann@gmail.com>
2010-04-21 15:44:26 +0200

    version bump to 1.0.0 - finally? :D

--- lib/vacuum_cleaner.rb
+++ lib/vacuum_cleaner.rb
@@ -5,7 +5,7 @@
 # @see VacuumCleaner::Normalizer
 module VacuumCleaner
   # +VacuumCleaner+ version
-  VERSION = "0.5.0".freeze
+  VERSION = "1.0.0".freeze
   
   autoload :Normalizer, 'vacuum_cleaner/normalizer'
   autoload :Normalizations, 'vacuum_cleaner/normalizations'

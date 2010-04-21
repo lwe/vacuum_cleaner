70a31d94fd0be037254a775c69b28f12aebbc075
lukas <lukas.westermann@gmail.com>
2010-04-21 15:11:21 +0200

    fixed init files for Rails

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,3 @@
 require 'vacuum_cleaner'
 
 ActiveRecord::Base.class_eval { include VacuumCleaner::Normalizations } # all versions of rails
-# ActiveModel::Base.class_eval { include VacuumCleaner::Normalizations } if defined?(ActiveModel::Base) # Rails 3+

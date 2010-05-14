16a8ded6130af090ac1ba1482aa559f659d3e205
lukas <lukas.westermann@gmail.com>
2010-05-14 11:37:12 +0200

    added initial version of NumericNormalizer

---
+++ lib/vacuum_cleaner/normalizations/numeric.rb
@@ -0,0 +1,39 @@
+module VacuumCleaner
+  module Normalizations
+    
+    # The numeric normalizer tries to normalize a numeric string input
+    # like <tt>CHF 12'000.--</tt> into a string parseable by {{Kernel.Float}}
+    # or similar (result would be <tt>12000.</tt>).
+    #
+    # It basically strips any non valid character for a number from the
+    # input string (scientific notation is currently not support), so all
+    # whitespace, currency symbols and units are stripped. Furthermore also
+    # the decimal points are normalized, because in Germany numbers can look
+    # like: <tt>EUR 12.000,50</tt> and in Switzerland they can look like
+    # <tt>1,5 Mio.</tt>. So the normalizer tries to ensure that both are
+    # numbers parseable by {{Kernel.Float}}, by intelligently trying to
+    # figure out the seperator used and converting it to <tt>.</tt>.
+    #
+    # All values which do not respond to <tt>to_str</tt> are left as is.
+    #
+    # Note: no conversion or anything similar is done! The value wont be
+    # converted to a +Fixnum+ or whatever and will be left as string.
+    # When used with Rails validations, this might also certainly render
+    # the validations to check if it's a valid number obsolete, yet all
+    # the stuff about min/max, fixed only etc. work of course like a charm.
+    #
+    class NumericNormalizer < Normalizer
+      def normalize_value(value)
+        if value.respond_to?(:to_str)
+          num = value.to_str.gsub(/\s*/, '') # I. remove all spaces
+          num.gsub!(/([^\d\-])\./, '\1')     # II. remove misleading points in units like "Mio." or "SFr."
+          num.gsub!(/[^\d,\.\-]/i, '')       # III. remove all chars we are not interested in, like anything not related to numeric :)
+          num.gsub!("--", '')                # IV. remove double dashes, like often used in CH
+          num.gsub(",", '.').gsub(".") { $'.include?(".") ? "" : "." }  # V. intelligently convert comma to points...          
+        else
+          value
+        end
+      end      
+    end
+  end
+end
---
+++ test/unit/vacuum_cleaner/normalizations/numeric_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+require 'vacuum_cleaner/normalizer'
+require 'vacuum_cleaner/normalizations/numeric'
+
+class VacuumCleaner::Normalizations::NumericTtest < Test::Unit::TestCase
+  include VacuumCleaner::Normalizations
+  
+  context "NumericNormalizer#normalize_value" do
+    should "remove any non numeric character, except decimal character" do
+      assert_equal "10.5", NumericNormalizer.new.normalize_value("10.5")
+      assert_equal "-12.3434", NumericNormalizer.new.normalize_value("-12.3434")      
+      assert_equal "1250.", NumericNormalizer.new.normalize_value("CHF 1250.--")
+      assert_equal "121250", NumericNormalizer.new.normalize_value("121'250 Mio. USD")
+    end
+    
+    should "accept a negative prefix" do
+      assert_equal "-450.00", NumericNormalizer.new.normalize_value("CHF -450.00")
+      assert_equal "-.50", NumericNormalizer.new.normalize_value("- .50 SFr.")
+    end
+    
+    should "work with German/Swiss notation of comma as decimal separator" do
+      assert_equal "40.50", NumericNormalizer.new.normalize_value("CHF 40,50")
+      assert_equal "1040.50", NumericNormalizer.new.normalize_value("EUR 1.040,50")
+      assert_equal "-1100040.50", NumericNormalizer.new.normalize_value("EUR -1.100.040,50")
+    end
+    
+    should "leave numeric and nil values as is" do
+      assert_nil NumericNormalizer.new.normalize_value(nil)
+      assert_equal 12.5, NumericNormalizer.new.normalize_value(12.5)
+    end
+  end  
+end

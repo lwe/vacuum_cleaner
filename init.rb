6879c6c17992fa1898655975ed72b71b811a61f3
lukas <lukas.westermann@gmail.com>
2010-09-09 17:03:59 +0200

    use AS.on_load callback for rails3 integration

--- init.rb
+++ init.rb
@@ -1,5 +1,1 @@
 require 'vacuum_cleaner'
-
-if Object.const_defined?("ActiveRecord")
-  ActiveRecord::Base.send(:include, VacuumCleaner::Normalizations)
-end
--- lib/vacuum_cleaner.rb
+++ lib/vacuum_cleaner.rb
@@ -1,12 +1,19 @@
 # Fancy value normalization utility for ruby (and rails),
 # see {VacuumCleaner::Normalizations} for more information about usage.
 #
+
+require 'vacuum_cleaner/normalizer'
+require 'vacuum_cleaner/normalizations'
+
 # @see VacuumCleaner::Normalizations
 # @see VacuumCleaner::Normalizer
 module VacuumCleaner
   # +VacuumCleaner+ version
   VERSION = "1.0.0".freeze
-  
-  autoload :Normalizer, 'vacuum_cleaner/normalizer'
-  autoload :Normalizations, 'vacuum_cleaner/normalizations'
+end
+
+if defined?(ActiveSupport) && ActiveSupport.respond_to?(:on_load)
+  ActiveSupport.on_load(:active_record) { include VacuumCleaner::Normalizations }
+else
+  ActiveRecord::Base.send(:include, VacuumCleaner::Normalizations) if defined?(ActiveRecord)  
 end

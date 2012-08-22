2e4c1c326b9955e2b880684d2b289e55e8f462de
lukas <lukas.westermann@gmail.com>
2012-08-22 11:59:27 +0200

    use autoload

--- lib/vacuum_cleaner.rb
+++ lib/vacuum_cleaner.rb
@@ -2,13 +2,13 @@
 # see {VacuumCleaner::Normalizations} for more information about usage.
 #
 
-require 'vacuum_cleaner/normalizer'
-require 'vacuum_cleaner/normalizations'
-
 # @see VacuumCleaner::Normalizations
 # @see VacuumCleaner::Normalizer
 module VacuumCleaner
   autoload :VERSION, 'vacuum_cleaner/version'
+
+  autoload :Normalizer, 'vacuum_cleaner/normalizer'
+  autoload :Normalizations, 'vacuum_cleaner/normalizations'
 end
 
 if defined?(ActiveSupport) && ActiveSupport.respond_to?(:on_load)
